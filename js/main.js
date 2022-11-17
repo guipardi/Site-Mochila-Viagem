721a2744d0ad84fc3f5aab18c21bf14ffa91b6c3
guipardi <guilhermepardi@gmail.com>
2022-11-17 16:39:50 -0300

    Atualizando a parte do localStorage.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,44 @@
 const form = document.getElementById('novo-item');
 const lista = document.querySelector('.lista');
-const itens = []
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
+
+/* Percorrendo todos os intens do localStorage, assim que a página é carregada, e adicionando todos eles usando a função criaElemento. */
+itens.forEach( (elemento) => {
+   criaElemento(elemento)
+});
 
 form.addEventListener('submit', (event) => {
 
     event.preventDefault();
 
-    /* console.log(event.target.elements['nome'].value);
-    console.log(event.target.elements['quantidade'].value);*/
-
-    /* criaElemento(event.target.elements['nome'].value, event.target.elements['quantidade'].value) */
-
+    /* Buscando os dados passados no formulário. */
     const nome = event.target.elements['nome'].value;
     const quantidade = event.target.elements['quantidade'].value;
-    adicionaElemento(criaElemento(nome, quantidade), nome, quantidade);
-
-    form.reset();
-});
-
-function criaElemento(nome, quantidade) {
-    const novoItem = document.createElement('li');
-    novoItem.classList.add('item');
     
-    const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
-
-    novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
-
-    return novoItem;
-};
-
-function adicionaElemento(elemento, nome, quantidade) {
-    lista.appendChild(elemento);
-
+    /* Criando o objeto para criar o elemento, adiciona-lo na lista itens, e setar no localStorage a lista itens. */
     const itemAtual = {
         'nome' : nome,
         'quantidade' : quantidade
     };
+    
+    criaElemento(itemAtual);
 
     itens.push(itemAtual);
 
-    localStorage.setItem('item', JSON.stringify(itens));
+    localStorage.setItem('itens', JSON.stringify(itens));
+
+    form.reset();
+});
+
+function criaElemento(item) {
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item');
+    
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = item.quantidade;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += item.nome;
+
+    lista.appendChild(novoItem)
 };
